25786c58acc70575433e6038028cbfd33d7060a5
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-10 05:58:20 -0300

    Redesigned objects and trying move the sphere on the cube

---
+++ Assets/Vuforia/Scripts/SphereController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+    public float speed;
+    public GameObject cube;
+    private Rigidbody rb;
+    private float distanceToTheBorder;
+    private bool goToTheRight;
+
+	// Use this for initialization
+	void Start () {
+        goToTheRight = true;
+        rb = GetComponent<Rigidbody>();
+        RectTransform rt = cube.GetComponent<RectTransform>();
+        distanceToTheBorder = (rt.localScale.x * rt.sizeDelta.x) / 2;
+	}
+	
+	void FixedUpdate () {
+		if (goToTheRight)
+        {
+            Vector3 movement = new Vector3(distanceToTheBorder, 0.0f, 0.0f);
+            rb.AddForce(movement * speed);
+
+            if (IsInRightBorder()) goToTheRight = false;
+        }
+        else
+        {
+            Vector3 movement = new Vector3(-distanceToTheBorder, 0.0f, 0.0f);
+            rb.AddForce(movement * speed);
+
+            if (IsInLeftBorder()) goToTheRight = true;
+        }
+	}
+
+    private bool IsInRightBorder ()
+    {
+        return transform.position.x == distanceToTheBorder;
+    }
+
+    private bool IsInLeftBorder ()
+    {
+        return transform.position.x == - distanceToTheBorder;
+    }
+}
