f562c12ffdd7e4a2e1d544e92a2ea4f78b2a343b
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-12 04:34:54 -0300

    Implemented movement of the sphere on the cube

--- Assets/Vuforia/Scripts/SphereController.cs
+++ Assets/Vuforia/Scripts/SphereController.cs
@@ -1,47 +1,44 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SphereController : MonoBehaviour {
-
-    public float speed;
-    public GameObject cube;
+public class SphereController : MonoBehaviour
+{
+    public float horizontalAcceleration;
+    public GameObject ground;
     private Rigidbody rb;
     private float distanceToTheBorder;
-    private bool goToTheRight;
 
-	// Use this for initialization
-	void Start () {
-        goToTheRight = true;
-        rb = GetComponent<Rigidbody>();
-        RectTransform rt = cube.GetComponent<RectTransform>();
-        distanceToTheBorder = (rt.localScale.x * rt.sizeDelta.x) / 2;
-	}
-	
-	void FixedUpdate () {
-		if (goToTheRight)
-        {
-            Vector3 movement = new Vector3(distanceToTheBorder, 0.0f, 0.0f);
-            rb.AddForce(movement * speed);
-
-            if (IsInRightBorder()) goToTheRight = false;
-        }
-        else
-        {
-            Vector3 movement = new Vector3(-distanceToTheBorder, 0.0f, 0.0f);
-            rb.AddForce(movement * speed);
-
-            if (IsInLeftBorder()) goToTheRight = true;
-        }
-	}
-
-    private bool IsInRightBorder ()
+    // Use this for initialization
+    void Start()
     {
-        return transform.position.x == distanceToTheBorder;
+        rb = GetComponent<Rigidbody>();
+        RectTransform rt = ground.GetComponent<RectTransform>();
+        distanceToTheBorder = (rt.localScale.x * rt.sizeDelta.x) / 2;
+        rb.AddForce(horizontalAcceleration, 0, 0);
     }
 
-    private bool IsInLeftBorder ()
+    private void FixedUpdate()
     {
-        return transform.position.x == - distanceToTheBorder;
+        if (IsInRightBorder())
+        {
+            rb.AddForce(-horizontalAcceleration * 2, 0, 0);
+        }
+
+        else if (IsInLeftBorder())
+        {
+            rb.AddForce(horizontalAcceleration * 2, 0, 0);
+        }
+    }
+
+    private bool IsInRightBorder()
+    {
+        return rb.transform.position.x >= distanceToTheBorder;
+    }
+
+    private bool IsInLeftBorder()
+    {
+        return rb.transform.position.x <= -distanceToTheBorder;
     }
 }
