ff038c267011f72e92cdf87187b20796f1524e67
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-08-12 21:08:43 -0300

    Target detection, light activation and start of motion of the sphere

---
+++ Assets/Scripts/Detect.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class Detect : MonoBehaviour, ITrackableEventHandler
+{
+    #region PRIVATE_MEMBER_VARIABLES
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    private bool lightIsActive;
+
+    #endregion // PRIVATE_MEMBER_VARIABLES
+
+    #region PUBLIC_MEMBER_VARIABLES
+
+    public new LightController light;
+    public GameObject player;
+
+    #endregion // PUBLIC_MEMBER_VARIABLES
+
+    #region UNTIY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+
+        lightIsActive = false;
+        player.SetActive(false);
+    }
+
+    #endregion // UNTIY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            lightIsActive = true;
+            player.SetActive(true);
+        }
+        else
+        {
+            lightIsActive = false;
+            player.SetActive(false);
+        }
+        
+        light.Change(lightIsActive);
+    }
+
+    #endregion // PUBLIC_METHODS
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightController : MonoBehaviour {
+
+    public void Change(bool activeState)
+    {
+        if (activeState)
+        {
+            GetComponent<Image>().color = Color.green;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.red;
+        }
+    }
+
+}
